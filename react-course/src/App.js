5fd16db6371257466e546579f10b539a4ed93101
Madhin Hussain <madhinhussain@gmail.com>
2023-05-04 17:37:47 +0100

    meetups app updated with Dummy Data

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,14 +1,24 @@
-
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <h1>My Todo's</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore full React course" />
-     
+      <MainNavigation/>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+          </Route>
+        </Switch>
     </div>
   );
 }
