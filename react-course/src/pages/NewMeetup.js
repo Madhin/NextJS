a855679832e21c04edd52ddaf4be1ac7f089c45c
Madhin Hussain <madhinhussain@gmail.com>
2023-05-05 16:17:33 +0100

    NewMeetupForm amended

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,9 +1,12 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetup) {
+
+    }
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm/>
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
 }
 
