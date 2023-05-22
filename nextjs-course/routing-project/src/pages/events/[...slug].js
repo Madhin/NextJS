10a99329004e9ab82d808fe21dc810f9b04bb0e0
Madhin Hussain <madhinhussain@gmail.com>
2023-05-22 15:41:21 +0100

    finished project

--- nextjs-course/routing-project/src/pages/events/[...slug].js
+++ nextjs-course/routing-project/src/pages/events/[...slug].js
@@ -1,9 +1,72 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+import EventList from "@/components/events/event-list";
+import ResultsTitle from "@/components/events/results-title";
+import { Fragment } from "react";
+import Button from "@/components/UI/button";
+import ErrorAlert from "@/components/UI/error-alert";
+
 function FilteredEvents() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  console.log(filterData);
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
     return (
-      <div>
-        <h1>Filtered Events</h1>
-      </div>
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid Filter, please adjust values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all Events</Button>
+        </div>
+      </Fragment>
     );
   }
-  
-  export default FilteredEvents;
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+}
+
+export default FilteredEvents;
